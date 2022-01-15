9172fd4e16439d50af829bd3f6df49bea5a346df
orozcoding <orozcoding@gmail.com>
2022-01-15 04:04:51 -0400

    add basic token display

--- js/app.js
+++ js/app.js
@@ -1,10 +1,9 @@
-import { checkCoins, coins, getCoins, pushCoins, getPrice, printPrice } from './functions.js';
+import { checkCoins, coins, getCoins, pushCoins, fetchPrice, getPrice, displayToken, removeToken, updatePrices } from './functions.js';
 import { checkerBoxes } from './containers.js';
 import { tokens } from './coins.js';
 
 window.addEventListener('load', () => {
   checkCoins();
-  console.log('checked the coins');
 })
 
 checkerBoxes.forEach((box) => {
@@ -12,17 +11,17 @@ checkerBoxes.forEach((box) => {
     let arr = Array.from(checkerBoxes)
     let coins = getCoins();
     let i = arr.indexOf(box);
-    console.log(i);
+    let token = coins[i];
     if(box.checked){
-      console.log(`checking ${coins[i].name}`);
-      coins[i].act = true;
-      pushCoins(coins);
-      let price = getPrice(coins[i]);
-      printPrice(price);
+      token.act = true;
+      setTimeout(pushCoins(coins), 2000) ;
+      displayToken(token);
+      updatePrices(coins);
     } else {
-      console.log(`unchecking ${coins[i].name}`);
       coins[i].act = false;
       pushCoins(coins);
+      removeToken(token);
     }
   })
 })
+
--- js/functions.js
+++ js/functions.js
@@ -1,6 +1,7 @@
 import { tokens, changeCoins } from './coins.js';
 import { checkerBoxes } from './containers.js';
 
+let coinsContainer = document.getElementById('coins-container');
 let coins = [];
 
 function getCoins() {
@@ -20,17 +21,77 @@ function checkCoins() {
   }
 }
 
-function getPrice(coin) {
-  fetch(coin.api)
+function fetchPrice(token) {
+  let price = fetch(token.api)
     .then((response) => response.json())
     .then((data) => {
-      return data[coin.path];
+      return data[token.path]['usd'];
     });
+  return price;
 }
 
-async function printPrice(token) {
-  const a = await token;
-  console.log('el precio es ' + a);
+async function getPrice(token) {
+  let price = fetchPrice(token)
+  const a = await price;
+  return Number(a);
 };
 
-export { checkCoins, coins, getCoins, pushCoins, getPrice, printPrice };
+function updateBalance(name, balance) {
+  let coins = getCoins();
+  for (let i = 0; i < coins.length; i++) {
+    if (coins[i].name === name) {
+      coins[i].balance = Number(balance);
+    }
+  }
+  pushCoins(coins);
+}
+
+async function displayToken(coin) {
+  let newToken = document.createElement('div');
+  let { name } = coin;
+  newToken.id = `${coin.name}-container`;
+  newToken.classList.add('coin-container', 'd-flex');
+
+  newToken.innerHTML = `<div id="${name}-title" class="coin-title">${name.toUpperCase()}</div>
+  <div id="${name}-section" class="coin-usd">Price in USD:</div>
+  <div class="coin-price-container"><div id="${name}-price" class="coin-price">Loading Price...</div></div>
+  <input id="${name}-input" class="coin-input" type="number" onwheel="this.blur()">
+  <div id="${name}-balance" class="coin-balance">Enter your tokens</div>`
+
+  coinsContainer.appendChild(newToken);
+
+  let price = await getPrice(coin);
+  let priceBox = document.getElementById(`${name}-price`);
+  setTimeout(() => {
+    priceBox.innerHTML = '$' + price.toFixed(4);
+  }, 2000)
+
+  let balance = document.getElementById(`${name}-balance`)
+
+  let input = document.getElementById(`${name}-input`);
+  input.addEventListener('keyup', () => {
+    let newBalance = Number((input.value * price).toFixed(2));
+    balance.innerHTML = `$${newBalance}`;
+    updateBalance(name, newBalance);
+  })
+}
+
+function removeToken(coin) {
+  let toRemove = document.getElementById(`${coin.name}-container`);
+  toRemove.remove();
+}
+
+async function updatePrices(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].act === true) {
+      let price = await getPrice(arr[i]);
+      arr[i].price = price;
+    }
+  }
+  pushCoins(arr);
+}
+
+
+
+
+export { checkCoins, coins, getCoins, pushCoins, fetchPrice, getPrice, displayToken, removeToken, updatePrices };
